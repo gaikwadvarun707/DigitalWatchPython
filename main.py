bbd364fcbc7bee369855882156a827e47db204b6
varun gaikwad <gaikwadvarun707@gmail.com>
2021-06-09 14:52:29 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+
+root = Tk()
+root.title("Clock")
+
+def time():
+    str=strftime('%H:%M:%S %p')
+    label.config(text=str)
+    label.after(1000,time)
+
+label=Label(root, font=("ds-digital",80),background="black",foreground="cyan")
+label.pack(anchor='center')
+time()
+
+mainloop()
